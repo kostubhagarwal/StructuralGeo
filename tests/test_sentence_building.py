bec81b666199b45923cd740934ffd792a494e2be
Simon Ghyselincks <sghyselincks@gmail.com>
2024-07-26 00:24:49 -0700

    Data generator object for probabilistic decision tree sampling

--- tests/test_sentence_building.py
+++ tests/test_sentence_building.py
@@ -21,18 +21,33 @@ import random
 #     histories.extend(h)
 
 # ---- Single Sentence Testing ---- #    
-sentence = [InfiniteBasement(), FineRepeatSediment(), FineRepeatSediment(), MicroNoise() , NullWord()]
-histories = generate_history(sentence, 16)    
+def single_sentence_test():
+    sentence = [InfiniteBasement(), FineRepeatSediment(), FineRepeatSediment(), MicroNoise() , NullWord()]
+    histories = [generate_history(sentence) for _ in range(16)]  
+    # Select a random set of 16 histories
+    selected_histories = random.sample(histories, 16)
+    p = pv.Plotter(shape=(4, 4))    
+    for i, hist in enumerate(selected_histories):
+        p.subplot(i // 4, i % 4)
+        model = generate_normalized_model(hist)
+        geovis.volview(model, plotter=p)    
+    p.show()
+
+yaml_loc = 'C:/Users/sghys/Summer2024/StructuralGeo/src/structgeo/generation/grammar_map.yml'    
+def model_loader_test():
+    loader = GeologicalModelLoader(yaml_loc, model_resolution=(128,128,64)) 
+    models = loader.generate_model_batch(16) 
+    p = pv.Plotter(shape=(4, 4))
+    for i, model in enumerate(models):
+        p.subplot(i // 4, i % 4)
+        geovis.volview(model, plotter=p)
+    p.show()
+   
+
+    print('')
     
-# Select a random set of 16 histories
-selected_histories = random.sample(histories, 16)
-p = pv.Plotter(shape=(4, 4))    
-for i, hist in enumerate(selected_histories):
-    p.subplot(i // 4, i % 4)
-    model = generate_normalized_model(hist)
-    geovis.volview(model, plotter=p)
     
-p.show()
+model_loader_test()
     
 
 
