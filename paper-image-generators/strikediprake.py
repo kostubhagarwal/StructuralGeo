1f9500135120c14da7fe0d4618a3dcff07aad901
Simon Ghyselincks <sghyselincks@gmail.com>
2024-08-08 10:59:09 -0700

    GeoProcess documention, source code for images, images, icon svgs, drawio, and pdfs

--- paper-image-generators/strikediprake.py
+++ paper-image-generators/strikediprake.py
@@ -3,17 +3,17 @@ import pyvista as pv
 from structgeo.model import rotate
 
 # Setup plotter
-p = pv.Plotter(window_size=[800,600], off_screen=True)
+p = pv.Plotter(off_screen=True)
 
-xy_plane = pv.Plane(center=(0, 0, 0), direction=(0,0,1), i_size=8, j_size=10, i_resolution=4, j_resolution=4,)
+xy_plane = pv.Plane(center=(0, 0, 0), direction=(0.,0,1), i_size=8, j_size=10, i_resolution=4, j_resolution=4,)
 p.add_mesh(pv.Sphere(radius=.3, center=xy_plane.center), color='red')  # Center point
 p.add_mesh(xy_plane, color='lightgreen', opacity = .5, show_edges=True)
 # p.add_floor()
 northing_arrow = pv.Arrow(start=xy_plane.center, direction=(0, 1, 0), scale=8, tip_length=.20, tip_radius=0.03, shaft_radius=0.01,)
 p.add_mesh(northing_arrow, color='black', label='Northing Vector')
-p.add_point_labels((-3,-3,4), ["Feature Plane"], italic=False, bold=True, show_points=False, font_size=16, text_color='black',
+p.add_point_labels((-3.,-3,4), ["Feature Plane"], italic=False, bold=True, show_points=False, font_size=32, text_color='black',
                    shape_color='grey', shape='rounded_rect', fill_shape=True, margin=3, shape_opacity=.1,)
-p.add_point_labels((3,-1,0), ["Horizon Plane"], italic=False, bold=True, show_points=False, font_size=16, text_color='black',
+p.add_point_labels((3.,-1,0), ["Horizon Plane"], italic=False, bold=True, show_points=False, font_size=32, text_color='black',
                    shape_color='grey', shape='rounded_rect', fill_shape=True, margin=3, shape_opacity=.1,)
 
 # Add arrow for strike
@@ -28,7 +28,7 @@ dip =  50
 rake = 65
 n_vec = rotate(strike_vec, dip*np.pi/180) @ [0,0,1]
 trans_plane = pv.Plane(center=(0, 0, 0), direction=n_vec, i_size=10, j_size=10)
-trans_plane.rotate_vector(vector=n_vec, angle=rake, point=(0,0,0))
+trans_plane.rotate_vector(vector=n_vec, angle=rake, point=(0.,0,0))
 p.add_mesh(trans_plane, color='lightblue', opacity = .5, show_edges=True)
 
 # Compute and display intersection line
@@ -42,26 +42,39 @@ p.add_mesh(pv.Arrow(start=xy_plane.center, direction=dip_vec,  scale=5, tip_leng
 
 p.add_mesh(pv.Arrow(start=xy_plane.center, direction=n_vec,  scale=5, tip_length=.25, tip_radius=0.05, shaft_radius=0.02), color='blue', label='Normal Vector')
 
-p.add_legend(bcolor='white', border=True, size=[0.2, 0.2], loc = 'center right')
+p.add_legend(bcolor='white', border=True, size=[0.4, 0.4], loc = 'center right')
 
-_ = p.add_axes(line_width=6)
+# Add axes with increased size
+_ = p.add_axes(line_width=6, labels_off=False, color='black',               
+                shaft_length=1,
+                tip_length=.3,
+                ambient=0.2,
+                label_size=(.4, 0.24),)
+
 p.show_bounds(
     grid=False, 
     location='outer', 
     ticks='outside',
-    n_xlabels=0, 
-    n_ylabels=0, 
-    n_zlabels=0,  # Set number of z labels to 0 to hide z-axis labels
     font_size=12,
     show_zlabels=False,  # Do not show z labels
+    show_xlabels=False,
+    show_ylabels=False,
     xtitle='Easting', 
     ytitle='Northing',  
     ztitle=''  # No title for z-axis
 )
 
 p.view_vector([1,-1,0.6])
+
+# 
+# Get the current camera position
+position, focal_point, view_up = p.camera_position
+
+# Update the camera position to translate the view
+new_position = [position[0]+5, position[1]+5, position[2]-2]
+new_focal_point = [focal_point[0]+5, focal_point[1]+5, focal_point[2]]
+p.camera_position = [new_position, new_focal_point, view_up]
 # Increase the camera distance to zoom out
 p.camera.zoom(.8)
 
-p.show()
-p.screenshot("strike-dip-rake.png")
+p.screenshot("strike-dip-rake.png", scale=1, transparent_background=True)
